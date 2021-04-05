146dcf766d03be03f20df586a11120f00567f1dc
chealtae <1402242918@qq.com>
2021-04-05 21:30:00 +0800

    修改

--- src/renderer/components/Common/playBar.vue
+++ src/renderer/components/Common/playBar.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="player">
 		<div class="songInfo">
-			<img src="../../assets/img/img1.jpg" alt="" class="songImg">
+			<img src="../../assets/img/img1.jpg" alt="" class="songImg" >
 			<span class="info_span">{{song.songName}}</span>
 			<img v-if="song.isCollect" src="../../assets/img/heart.svg" alt=""  class="heartImg" title="取消喜欢" @click="collectSong">
 			<img v-else src="../../assets/img/heart1.svg" alt=""  class="heartImg" title="喜欢" @click="cancelCollect">
@@ -10,27 +10,27 @@
 		</div>
 		<div style="margin-top:8px">
 			<div>
-				<audio :src="currentSongSrc" id="audio" ></audio> 
+				<audio :src="currentSongSrc" ref="audio" ></audio> 
 			</div>
 			<div class="operation">
-				<img v-show="playStyle === 'circle'" title="单曲播放" src="../../assets/img/circlePlay.svg" alt="" @click="changeStyle">
-				<img v-show="playStyle === 'list'" title="列表播放" src="../../assets/img/listPlay.svg" alt="" @click="changeStyle">
-				<img v-show="playStyle === 'randon'" title="随机播放" src="../../assets/img/randonPlay.svg" alt="" @click="changeStyle">
-				<img src="../../assets/img/lastSong.svg" alt="" @click="lastMusic">
-				<img v-if="!isplay" src="../../assets/img/play.svg" alt="" @click="playMusic">
-				<img v-else src="../../assets/img/pause.svg" alt="" @click="pauseMusic">
-				<img src="../../assets/img/nextSong.svg" alt="" @click="nextMusic">
-				<img src="../../assets/img/lyric.svg" alt="" title="显示歌词">
+				<img v-show="playStyle === 'circle'" title="单曲播放" src="../../assets/img/circlePlay.svg" alt="" @click="changeStyle" class="operaIcon">
+				<img v-show="playStyle === 'list'" title="列表播放" src="../../assets/img/listPlay.svg" alt="" @click="changeStyle" class="operaIcon">
+				<img v-show="playStyle === 'randon'" title="随机播放" src="../../assets/img/randonPlay.svg" alt="" @click="changeStyle" class="operaIcon"> 
+				<img src="../../assets/img/lastSong.svg" alt="" @click="lastMusic" class="operaIcon">
+				<img v-if="!isplay" src="../../assets/img/play.svg" alt="" @click="playMusic" class="operaIcon">
+				<img v-else src="../../assets/img/pause.svg" alt="" @click="pauseMusic" class="operaIcon">
+				<img src="../../assets/img/nextSong.svg" alt="" @click="nextMusic" class="operaIcon">
+				<img src="../../assets/img/lyric.svg" alt="" title="显示歌词" class="operaIcon">
 			</div>
 			<div class="playBar">
-				<div class="time">00:00</div>
+				<div class="time">{{currentPlayTime}}</div>
 				<div class="silder" @click="onMousedown">
 					<div class="buffer" :style="{width:`${bufferOffsetWidth}px`}"></div>
 					<div class="processor"  :style="{width:`${processorOffsetWidth}px`}"></div>
 					<div class="controller"  v-drag='{}' 
 					:style="{transform : `translateX(${TranslateX}px)`, left:`${processorOffsetWidth}px`}"></div>
 				</div>
-				<div class="time">04:21</div>
+				<div class="time" >{{musicLength}}</div>
 			</div>
 		</div>
 		<div class="playOperation">
@@ -63,38 +63,20 @@ export default {
 			bufferOffsetWidth: 0,
 			controllerOffsetWidth: 0,
 			progressbarTranslateX: 0,
+			currentTime:'00:00',
+			musicTime:'00:00',
 			isplay:false,
 			playStyle:localStorage.getItem('playStyle')? localStorage.getItem('playStyle'):"list",//默认列表播放
 			song:{singId:1,songName:'爱就一个字11111',singer:'傲七爷',isCollect:false},
 			visible: false,
 			getSongList:[{singId:1},{singId:2},{singId:3}],
 			songSrc:[{src:require('@/assets/img/test.mp3')},{src:require('@/assets/img/test2.mp3')},{src:require('@/assets/img/test3.mp3')}],
-			currentSongSrc:require('@/assets/img/test.mp3'),
-		}
+			currentSongSrc:require('@/assets/img/test.mp3'),	
+			}
 	},
 	mounted() {
-		let audioPlay = document.getElementById('audio');
-		audioPlay.loop = false //默认静音循环播放初始状态
-		audioPlay.addEventListener('ended',()=> {
-			if(this.playStyle === 'list'){ //如果是列表播放，需要再播放完后更改歌曲列表
-				let playedSong = this.getSongList.shift();
-				this.getSongList.push(playedSong) //把播放完的音乐移动到数组尾部
-				// this.$axios.get(``).then((res) => {
-					
-				// })
-				this.currentSongSrc = this.songSrc[this.getSongList[0].singId-1].src; //todo 这里直接删掉 ，改用请求过来的数据
-				console.log(this.currentSongSrc);
-			} else if(this.playStyle === 'circle') {
-				audioPlay.loop = true //设置单曲循环播放
-			} else if(this.playStyle === 'randon') {
-				this.currentSongSrc = this.songSrc[Math.ceil(Math.random()*this.getSongList.length-1)].src //向下取整
-				console.log(this.currentSongSrc);
-			}
-			setTimeout(() => { //todo 异步后应该不需要再用定时器
-				audioPlay.play()
-			}, 150);
-			
-		})
+		this.endListener();
+		this.watchTime();
 	},
 	computed: {
 		processorOffsetWidth() {
@@ -102,6 +84,12 @@ export default {
 		},
 		TranslateX() {
 			return this.progressbarTranslateX
+		},
+		musicLength() {
+			return this.musicTime
+		},
+		currentPlayTime() {
+			return this.currentTime
 		}
 	},
 	methods: {
@@ -111,34 +99,30 @@ export default {
 			console.log('moveX',this.processMoveX)
 		},
 		playMusic(){
-			let player = document.getElementById('audio');
-			player.play()
+			this.$refs.audio.play()
 			this.isplay = true;
 		},
 		pauseMusic() {
-			let player = document.getElementById('audio');
-			player.pause()
+			this.$refs.audio.pause()
 			this.isplay = false;
 		},
 		lastMusic() {
-			let player = document.getElementById('audio');
 			let playedSong = this.getSongList.pop();
 			this.getSongList.unshift(playedSong) //把播放完的音乐移动到数组t头部
 			this.currentSongSrc = this.songSrc[this.getSongList[0].singId-1].src; //todo 这里直接删掉 ，改用请求过来的数据
 			console.log(11111,this.currentSongSrc)
 			setTimeout(() => { //todo 异步后应该不需要再用定时器
-				player.play()
+				this.$refs.audio.play()
 			}, 150);
 			this.isplay = true;
 		},
 		nextMusic() {
-			let player = document.getElementById('audio');
 			let playedSong = this.getSongList.shift();
 			this.getSongList.push(playedSong) //把播放完的音乐移动到数组尾部
 			this.currentSongSrc = this.songSrc[this.getSongList[0].singId-1].src; //todo 这里直接删掉 ，改用请求过来的数据
 			console.log(11111,this.currentSongSrc)
 			setTimeout(() => { //todo 异步后应该不需要再用定时器
-				player.play()
+				this.$refs.audio.play()
 			}, 150);
 			this.isplay = true;
 		},
@@ -159,6 +143,51 @@ export default {
 				this.playStyle = 'list'
 				localStorage.setItem('playStyle','list')
 			}
+		},
+		endListener() {
+			this.$refs.audio.loop = false //默认静音循环播放初始状态
+			this.$refs.audio.addEventListener('ended',() => {
+				if(this.playStyle === 'list'){ //如果是列表播放，需要再播放完后更改歌曲列表
+					let playedSong = this.getSongList.shift();
+					this.getSongList.push(playedSong) //把播放完的音乐移动到数组尾部
+					// this.$axios.get(``).then((res) => {
+						
+					// })
+					this.currentSongSrc = this.songSrc[this.getSongList[0].singId-1].src; //todo 这里直接删掉 ，改用请求过来的数据
+					console.log(this.currentSongSrc);
+				} else if(this.playStyle === 'circle') {
+					this.$refs.audio.loop = true //设置单曲循环播放
+				} else if(this.playStyle === 'randon') {
+					this.currentSongSrc = this.songSrc[Math.ceil(Math.random()*this.getSongList.length-1)].src //向下取整
+					console.log(this.currentSongSrc);
+				}
+				setTimeout(() => { //todo 异步后应该不需要再用定时器
+					audioPlay.play()
+				}, 150);
+				
+			})
+		},
+		//监听时间变化
+		watchTime() {
+			this.$refs.audio.addEventListener('timeupdate',() => {
+				let timeDisplay = Math.floor(this.$refs.audio.currentTime)
+				let timeDisplay2 = Math.floor(this.$refs.audio.duration)
+				this.currentTime = this.processTime(timeDisplay)
+				this.musicTime = this.processTime(timeDisplay2)
+			});
+			console.log('xxxxxxxxxxxxxxxxxx')
+			this.$refs.audio.addEventListener('durationchange ', () => {
+				console.log('22222222222222222222')
+				let timeDisplay = Math.floor(this.$refs.audio.duration)
+				this.musicTime = this.processTime(timeDisplay)
+			})
+		},
+		processTime(timeDate) {
+			let minute =  Math.floor(timeDate / 60);
+			let second = timeDate % 60;
+			let secondPart = second < 10 ? '0'+second : second;
+			let minutePart = minute < 10 ? '0'+minute : minute;
+			return minutePart +':'+ secondPart;
 		}
 	},
 	directives:{
@@ -272,5 +301,8 @@ export default {
 		text-align: center;
 		width: 32px;
 		margin-top: 16px;
+	}
+	.operaIcon{
+		cursor: pointer;
 	}
 </style>
